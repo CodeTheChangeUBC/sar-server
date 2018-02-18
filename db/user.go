1e720208e41072536c253ca630583bb0818b2817
Lucas Salibian <lucas@freegeekvancouver.org>
2018-02-17 17:55:34 -0800

    Added user query functions

--- db/user.go
+++ db/user.go
@@ -31,9 +31,44 @@ type User struct {
 	Joined time.Time
 }
 
+// GetUserByID gets the given user by its unique ID.
+func GetUserByID(uid int) (User, error) {
+	row, err := Database.QueryRow("SELECT * FROM users WHERE id = ?", uid)
+	if err != nil {
+		return User{}, err
+	}
+
+	user, err := readUser(row)
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
+
 // A Coordinate is a set of the latitude, longitude and when the volunteer was
 // at that position.
 type Coordinate struct {
 	When  time.Time
 	Point Point
+}
+
+// Reads a user in from the given scanner
+func readUser(sc scanner) (User, error) {
+	var id int
+	var name string
+	var joined time.Time
+
+	err := sc.Scan(&id, &name, &joined)
+	if err != nil {
+		return User{}, nil
+	}
+
+	user := User{
+		ID:     id,
+		Name:   name,
+		Joined: joined,
+	}
+
+	return user, nil
 }
